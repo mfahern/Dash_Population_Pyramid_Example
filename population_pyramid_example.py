b45cc8ac9e6ba8891f7cd067a7b91b07a03d8d8a
mfahern <68118173+mfahern@users.noreply.github.com>
2024-04-30 17:07:24 -0500

    working match-case for all 10 countries

--- population_pyramid_example.py
+++ population_pyramid_example.py
@@ -10,33 +10,54 @@ app = Dash()
 
 app.layout = html.Div(
     children=[
+        dcc.Dropdown(['India','China','United States of America','Indonesia','Pakistan','Nigeria','Brazil','Bangladesh','Russia','Mexico'], 'India', id='country_dropdown'),
         html.H4("Population Pyramid"),
         dcc.Loading(
-            id="loading",
+            id='loading',
             type="cube",
-            children=dcc.Graph(id="graph")
-        )
+            children=dcc.Graph(id='graph')
+        ),
     ]
 )
 
 @app.callback(
-    Output("graph", "figure"),
-    Input("loading", "loading_states")
+    Output('graph', 'figure'),
+    Input('country_dropdown', 'value'),
 )
 
-def display_animated_graph(loading_states):
-    India_men_women = UN_10_largest_countries_pop_estimates_df.loc[UN_10_largest_countries_pop_estimates_df['Country'] == 'India']
-    China_men_women = UN_10_largest_countries_pop_estimates_df.loc[UN_10_largest_countries_pop_estimates_df['Country'] == 'China']
-    Brazil_men_women = UN_10_largest_countries_pop_estimates_df.loc[UN_10_largest_countries_pop_estimates_df['Country'] == 'Brazil']
-    return px.bar(
-        India_men_women,
-        x="Value",
-        y="Age",
-        animation_frame="Time",
-        orientation="h",
-        color="Sex",
-        height=1000,
-    )
+def create_country_bar(value):
+    match value:
+        case 'China':
+            country_selected = UN_10_largest_countries_pop_estimates_df.loc[UN_10_largest_countries_pop_estimates_df['Country'] == 'China']
+            population_pyramid_fig = px.bar(country_selected, x="Value", y="Age", animation_frame="Time", orientation="h", color="Sex", height=1000,)
+        case 'India':
+            country_selected = UN_10_largest_countries_pop_estimates_df.loc[UN_10_largest_countries_pop_estimates_df['Country'] == 'India']
+            population_pyramid_fig = px.bar(country_selected, x="Value", y="Age", animation_frame="Time", orientation="h", color="Sex", height=1000,)
+        case 'United States of America':
+            country_selected = UN_10_largest_countries_pop_estimates_df.loc[UN_10_largest_countries_pop_estimates_df['Country'] == 'United States of America']
+            population_pyramid_fig = px.bar(country_selected, x="Value", y="Age", animation_frame="Time", orientation="h", color="Sex", height=1000,)
+        case 'Indonesia':
+            country_selected = UN_10_largest_countries_pop_estimates_df.loc[UN_10_largest_countries_pop_estimates_df['Country'] == 'Indonesia']
+            population_pyramid_fig = px.bar(country_selected, x="Value", y="Age", animation_frame="Time", orientation="h", color="Sex", height=1000,)
+        case 'Pakistan':
+            country_selected = UN_10_largest_countries_pop_estimates_df.loc[UN_10_largest_countries_pop_estimates_df['Country'] == 'Pakistan']
+            population_pyramid_fig = px.bar(country_selected, x="Value", y="Age", animation_frame="Time", orientation="h", color="Sex", height=1000,)
+        case 'Nigeria':
+            country_selected = UN_10_largest_countries_pop_estimates_df.loc[UN_10_largest_countries_pop_estimates_df['Country'] == 'Nigeria']
+            population_pyramid_fig = px.bar(country_selected, x="Value", y="Age", animation_frame="Time", orientation="h", color="Sex", height=1000,)
+        case 'Brazil':
+            country_selected = UN_10_largest_countries_pop_estimates_df.loc[UN_10_largest_countries_pop_estimates_df['Country'] == 'Brazil']
+            population_pyramid_fig = px.bar(country_selected, x="Value", y="Age", animation_frame="Time", orientation="h", color="Sex", height=1000,)
+        case 'Bangladesh':
+            country_selected = UN_10_largest_countries_pop_estimates_df.loc[UN_10_largest_countries_pop_estimates_df['Country'] == 'Bangladesh']
+            population_pyramid_fig = px.bar(country_selected, x="Value", y="Age", animation_frame="Time", orientation="h", color="Sex", height=1000,)
+        case 'Russia':
+            country_selected = UN_10_largest_countries_pop_estimates_df.loc[UN_10_largest_countries_pop_estimates_df['Country'] == 'Russian Federation']
+            population_pyramid_fig = px.bar(country_selected, x="Value", y="Age", animation_frame="Time", orientation="h", color="Sex", height=1000,)
+        case 'Mexico':
+            country_selected = UN_10_largest_countries_pop_estimates_df.loc[UN_10_largest_countries_pop_estimates_df['Country'] == 'Mexico']
+            population_pyramid_fig = px.bar(country_selected, x="Value", y="Age", animation_frame="Time", orientation="h", color="Sex", height=1000,)
+    return population_pyramid_fig
 
 def main() -> None:
     app.run()
